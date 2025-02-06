4397200a1666df635ff71b8904d241bfd6036d6a
belkotproject <belkotproject@gmail.com>
2025-02-06 19:36:22 +0100

    Craeted a basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpServer, HttpResponse, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to the server")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind(("188.245.245.17", 80))?
+    .run()
+    .await
 }
