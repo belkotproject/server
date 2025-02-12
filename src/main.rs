f7c567dc05204c6c20ddb357ee36679e63156e37
belkotproject <belkotproject@gmail.com>
2025-02-12 15:34:07 +0100

    adding a fallback for incorrect urls

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ async fn index() -> impl Responder {
     HttpResponse::Ok().body("Welcome to the server")
 }
 
+async fn catch_all() -> impl Responder {
+    HttpResponse::Ok().body("HELLo")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
+            .route("/{tail:.*}", web::get().to(catch_all))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
